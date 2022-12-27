4b7394dd35c70c77ced60bd8fc6574f2eea63b41
Chris Wallace <Chrisw1096@gmail.com>
2022-12-27 14:48:49 +1100

    fuctions to convert ms to kts

--- script.js
+++ script.js
@@ -2,10 +2,18 @@
 let ApiKey = '770c17c22dff37637fc3801ca2553323';
 let lat = '-43.022219';
 let lon = '147.925758';
-let temp;
-let wind;
-// TO DO: 
+let temp = $('#temp');
+let windSpeed =$('#windSpeed');
+let windGusts =$('#windGusts');
+let windDeg =$('#windDeg');
+// Fuction to Change m/s to Knots
+function ms_kts(ms){
+ms=1.9438*ms;
+return ms
+}
+// Function to change Deg to cardinal points
 
+// Querying the URL
 let QueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&appid=" + ApiKey + "&units=metric";
 fetch(QueryURL)
 .then(function (response) {
@@ -14,10 +22,17 @@ fetch(QueryURL)
 
 .then(function (data) {
    
-
+console.log(data)
 // Insert current temp data into apporpriate ID's
-temp.html(data.current.temp + " celcius")
-wind.html(data.current.wind_speed + " m/s");
+temp.html(data.current.temp + " celcius");
+
+let WINDSPEED = ms_kts(data.current.wind_speed);
+let WINDGUST = ms_kts(data.current.wind_gust);
+let WINDDEG = ms_kts(data.current.wind_deg);
+
+windSpeed.html(WINDSPEED + " m/s");
+windGusts.html(WINDGUST + " m/s");
+windDeg.html(WINDDEG + " m/s");
 // humidity.html(data.current.humidity + " %")
 // uvIndex.html(data.daily[0].uvi)
 
