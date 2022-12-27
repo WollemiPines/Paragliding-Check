a1d3a9a6a2bd86f3dcfc7c15101ab87a61cef343
Chris Wallace <Chrisw1096@gmail.com>
2022-12-27 16:21:20 +1100

    fuctions to convert deg to cardinal

--- script.js
+++ script.js
@@ -8,10 +8,60 @@ let windGusts =$('#windGusts');
 let windDeg =$('#windDeg');
 // Fuction to Change m/s to Knots
 function ms_kts(ms){
-ms=1.9438*ms;
+ms= Math.round(1.9438*ms * 10) / 10
 return ms
 }
 // Function to change Deg to cardinal points
+function deg_card(deg){
+    if (deg>348.75, deg<11.25){
+     return "N"
+    }
+    if (deg>11.25, deg<33.75){
+    return "NNE"
+    }
+    if (deg>33.75, deg<56.25){
+    return "NE"
+    }
+    if (deg>56.25, deg<78.75){
+    return "ENE"
+    }
+    if (deg>78.75, deg<101.25){
+    return "E"
+    }
+    if (deg>101.25, deg<123.75){
+    return "ESE"
+    }
+    if (deg>123.75, deg<146.25){
+    return "SE"
+    }
+    if (deg>146.25, deg<168.75){
+    return "SSE"
+    }
+    if (deg>168.75, deg<191.25){
+    return "S"
+    }
+    if (deg>191.25, deg<213.75){
+    return "SSW"
+    }
+    if (deg>213.75, deg<236.25){
+    return "SW"
+    }
+    if (deg>236.25, deg<258.75){
+    return "WSW"
+    }
+    if (deg>258.75, deg<281.25){
+    return "W"
+    }
+    if (deg>281.25, deg<303.75){
+    return "WNW"
+    }
+    if (deg>303.75, deg<326.25){
+    return "NW"
+    }
+    if (deg>326.25, deg<348.75){
+    return "NNW"
+    }
+}
 
 // Querying the URL
 let QueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&appid=" + ApiKey + "&units=metric";
@@ -28,11 +78,11 @@ temp.html(data.current.temp + " celcius");
 
 let WINDSPEED = ms_kts(data.current.wind_speed);
 let WINDGUST = ms_kts(data.current.wind_gust);
-let WINDDEG = ms_kts(data.current.wind_deg);
+let WINDDEG = deg_card(data.current.wind_deg);
 
-windSpeed.html(WINDSPEED + " m/s");
-windGusts.html(WINDGUST + " m/s");
-windDeg.html(WINDDEG + " m/s");
+windSpeed.html(WINDSPEED + " Kts");
+windGusts.html(WINDGUST + " Kts");
+windDeg.html(WINDDEG );
 // humidity.html(data.current.humidity + " %")
 // uvIndex.html(data.daily[0].uvi)
 
