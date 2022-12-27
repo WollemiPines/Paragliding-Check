e9482531656da434adbc4081a49ef873081a7866
Chris Wallace <Chrisw1096@gmail.com>
2022-12-27 18:22:21 +1100

    colour picker classes

--- JS/helpers.js
+++ JS/helpers.js
@@ -1,4 +1,12 @@
 // data means current and future weather
 function colourpicker(data) {
-    if (data.dir = )
+    if (data.cardinalDir = ideal){
+        favColour.removeClass('ideal ok bad').addClass('ideal');
+    }
+    if (data.cardinalDir = ok){
+        favColour.removeClass('ideal ok bad').addClass('ok');
+    }
+    if (data.cardinalDir = bad){
+        favColour.removeClass('ideal ok bad').addClass('bad');
+    }
 }
