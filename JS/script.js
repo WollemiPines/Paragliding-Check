692217054949380a3fb667368484d6c5ad230681
Chris Wallace <Chrisw1096@gmail.com>
2022-12-28 14:51:26 +1100

    lst lon

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,7 @@
-// TO DO: Define Variables
+// Define Variables
 let ApiKey = '770c17c22dff37637fc3801ca2553323';
-let lat = '-43.022219';
-let lon = '147.925758';
+let lat ;
+let lon;
 let temp = $('#temp');
 let windSpeed =$('#windSpeed');
 let windGusts =$('#windGusts');
@@ -9,7 +9,62 @@ let windDeg =$('#windDeg');
 let humidity = $('#humidity');
 let uvIndex = $('#future_uvI0');
 
-// Fuction to Change m/s to Knots
+// Take user input and update lat and lon
+function userLatLon (lat, lon){
+lat = $('#searchLat').value;
+lon = $('#searchLon').value;
+// -43.022219
+// 147.925758
+
+}
+
+    
+// Querying the URL
+let QueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&appid=" + ApiKey + "&units=metric";
+fetch(QueryURL)
+.then(function (response) {
+    return response.json();
+  })
+
+.then(function (data) {
+   
+console.log(data)
+// Insert current data into apporpriate ID's
+temp.html(data.current.temp + " celcius");
+
+let WINDSPEED = ms_kts(data.current.wind_speed);
+let WINDGUST = ms_kts(data.current.wind_gust);
+let WINDDEG = deg_card(data.current.wind_deg);
+
+windSpeed.html(WINDSPEED + " Kts");
+windGusts.html(WINDGUST + " Kts");
+windDeg.html(WINDDEG );
+humidity.html(data.current.humidity + " %")
+uvColorClass()
+uvIndex.html(data.daily[0].uvi)
+
+
+// Pulls data from the daily querry and inserts into the #future_... ID tags
+for(let i=0; i<=6; i++){
+    $('#future_temp'+i).html(data.daily[i].temp.day + " celcius")
+    $('#future_wind'+i).html(data.daily[i].wind_speed + " m/s")
+//     $('#future_humidity'+i).html(data.daily[i].humidity + " %")
+//     $('#future_uvI'+i).html(data.daily[i].uvi)
+//     uvColorClass();
+//     $('#icon'+i).html(data.daily[i].weather[0].icon)
+//     iconFill();
+     }
+    })
+// ---------------------------------------------------------//
+// ---------------------------------------------------------//
+// ---------------------------------------------------------//
+                         // HELPERS //
+// ---------------------------------------------------------//
+// ---------------------------------------------------------//
+// ---------------------------------------------------------//
+
+
+    // Fuction to Change m/s to Knots
 const ms_kts = (
     function ms_kts(ms){
         ms= Math.round(1.9438*ms * 10) / 10
@@ -69,44 +124,19 @@ const ms_kts = (
         }
     }
     )
-    
-// Querying the URL
-let QueryURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=hourly,minutely,alerts&appid=" + ApiKey + "&units=metric";
-fetch(QueryURL)
-.then(function (response) {
-    return response.json();
-  })
 
-.then(function (data) {
-   
-console.log(data)
-// Insert current data into apporpriate ID's
-temp.html(data.current.temp + " celcius");
-
-let WINDSPEED = ms_kts(data.current.wind_speed);
-let WINDGUST = ms_kts(data.current.wind_gust);
-let WINDDEG = deg_card(data.current.wind_deg);
-
-windSpeed.html(WINDSPEED + " Kts");
-windGusts.html(WINDGUST + " Kts");
-windDeg.html(WINDDEG );
-humidity.html(data.current.humidity + " %")
-uvColorClass()
-uvIndex.html(data.daily[0].uvi)
-
-
-// Pulls data from the daily querry and inserts into the #future_... ID tags
-for(let i=0; i<=6; i++){
-    $('#future_temp'+i).html(data.daily[i].temp.day + " celcius")
-    $('#future_wind'+i).html(data.daily[i].wind_speed + " m/s")
-//     $('#future_humidity'+i).html(data.daily[i].humidity + " %")
-//     $('#future_uvI'+i).html(data.daily[i].uvi)
-//     uvColorClass();
-//     $('#icon'+i).html(data.daily[i].weather[0].icon)
-//     iconFill();
-     }
-    })
-
+    // data means current and future weather
+function colourpicker(data) {
+    if (data.cardinalDir = ideal){
+        favColour.removeClass('ideal ok bad').addClass('ideal');
+    }
+    if (data.cardinalDir = ok){
+        favColour.removeClass('ideal ok bad').addClass('ok');
+    }
+    if (data.cardinalDir = bad){
+        favColour.removeClass('ideal ok bad').addClass('bad');
+    }
+}
 
 // // change UV colours based on number value
 function uvColorClass(){
